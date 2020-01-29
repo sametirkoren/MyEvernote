6b44f2a35b653eb46feaa45316bf33a0824a8dde
Samet İrkören <sametirkoren@gmail.com>
2020-01-29 11:39:19 +0300

    DataAnnotations Entites

--- MyEvernote.Entities/Note.cs
+++ MyEvernote.Entities/Note.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyEvernote.Entities
 {
+    [Table("Notes")]
     public class Note : MyEntityBase
     {
+        [Required , StringLength(60)]
         public string Title { get; set; }
+        [Required, StringLength(2000)]
         public string Text { get; set; }
         public bool IsDraft { get; set; }
         public int LikeCount { get; set; }
@@ -18,6 +23,6 @@ namespace MyEvernote.Entities
         public virtual EvernoteUser Owner { get; set; }
         public virtual List<Comment> Comments { get; set; }
         public virtual Category Category { get; set; }
-        public  List<Liked> Likes { get; set; }
+        public virtual List<Liked> Likes { get; set; }
     }
 }
