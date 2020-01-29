f2b5c2f53e7bd660e1a297aaf7fe27cb30e8c641
Samet İrkören <sametirkoren@gmail.com>
2020-01-29 18:01:06 +0300

    Repository pattern

---
+++ MyEvernote.BusinessLayer/Repository.cs
@@ -0,0 +1,59 @@
+﻿using MyEvernote.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.BusinessLayer
+{
+    public class Repository<T> where T: class
+    {
+        private DatabaseContext db = new DatabaseContext();
+        private DbSet<T> _objectSet;
+        public Repository()
+        {
+            _objectSet = db.Set<T>();
+        }
+
+        public List<T> List()
+        {
+            // gelen tip neyse o tipin setini bul ve ona ToListi uygulana ve dönen listeyi geriye döndür.
+            return  _objectSet.ToList();
+        }
+
+        public int Insert(T obj)
+        {
+            _objectSet.Add(obj);
+            return Save();
+        }
+
+        public int Update(T obj)
+        {
+            return Save();
+        }
+
+        public int Delete(T obj)
+        {
+            _objectSet.Remove(obj);
+            return Save();
+        }
+
+        public List<T> List(Expression<Func<T,bool>>where)
+        {
+            return _objectSet.Where(where).ToList();
+        }
+
+        public int Save()
+        {
+            return db.SaveChanges();
+        }
+
+        public T Find(Expression<Func<T,bool>> where)
+        {
+            return _objectSet.FirstOrDefault(where);
+        }
+    }
+}
--- MyEvernote.BusinessLayer/Test.cs
+++ MyEvernote.BusinessLayer/Test.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyEvernote.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,41 @@ namespace MyEvernote.BusinessLayer
 {
     public class Test
     {
+        private Repository<EvernoteUser> repo_user = new Repository<EvernoteUser>();
+        private Repository<Category> repo = new Repository<Category>();
         public Test()
         {
-            DataAccessLayer.DatabaseContext db = new DataAccessLayer.DatabaseContext();
-            db.Categories.ToList();
+           
+            List<Category> categories = repo.List();
+        }
+
+        public void InsertTest()
+        {
+           
+            int result = repo_user.Insert(new EvernoteUser() { 
+                Name="aaa",
+                Surname="bbb",
+                Email="deneme@gmail.com",
+                ActivateGuid = Guid.NewGuid(),
+                IsActive = true , 
+                IsAdmin = true ,
+                Username = "aabb",
+                Password = "111",
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now.AddMinutes(5),
+                ModifiedUsername = "aabb"
+            });
+        }
+
+        public void UpdateTest()
+        {
+            EvernoteUser user = repo_user.Find(x => x.Username == "aabb");
+
+            if(user != null)
+            {
+                user.Username = "xxx";
+                int result = repo_user.Save();
+            }
         }
     }
 }
