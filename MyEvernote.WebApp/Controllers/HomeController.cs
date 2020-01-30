b3d5a87e3da609a1c6bc06e41d02474d121b2063
Samet İrkören <sametirkoren@gmail.com>
2020-01-30 13:49:14 +0300

    Notes List Page

--- MyEvernote.WebApp/Controllers/HomeController.cs
+++ MyEvernote.WebApp/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MyEvernote.BusinessLayer;
+using MyEvernote.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +11,33 @@ namespace MyEvernote.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+       
         public ActionResult Index()
         {
-            
-            return View();
+            // CategoryController üzerinden gelen view talebi ve model...
+            //if(TempData["mm"] != null)
+            //{
+            //    return View(TempData["mm"] as List<Note>);
+            //}
+            NoteManager nm = new NoteManager();
+            return View(nm.GetAllNote());
+        }
+
+        public ActionResult ByCategory(int? id)
+        {
+            if(id== null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CategoryManager cm = new CategoryManager();
+            Category cat = cm.GetCategoryById(id.Value);
+
+            if(cat == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("Index", cat.Notes);
         }
     }
 }
