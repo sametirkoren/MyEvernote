00d151ca30070bdd4a48eaf853ac507816217cf1
Samet İrkören <sametirkoren@gmail.com>
2020-02-18 23:54:19 +0300

    comments update

--- MyEvernote.WebApp/Controllers/CommentController.cs
+++ MyEvernote.WebApp/Controllers/CommentController.cs
@@ -13,6 +13,7 @@ namespace MyEvernote.WebApp.Controllers
     public class CommentController : Controller
     {
         private NoteManager noteManager = new NoteManager();
+        private CommentManager commentManager = new CommentManager();
 
         public ActionResult ShowNoteComments(int? id)
         {
@@ -27,6 +28,33 @@ namespace MyEvernote.WebApp.Controllers
                 return HttpNotFound();
             }
             return PartialView("_PartialComments",note.Comments);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id, string text)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = commentManager.Find(x => x.Id == id);
+
+            if (comment == null) 
+            {
+                return new HttpNotFoundResult();
+            }
+
+            comment.Text = text;
+
+
+            if (commentManager.Update(comment) > 0)
+            {
+                return Json(new {result = true}, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new {result = false}, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
